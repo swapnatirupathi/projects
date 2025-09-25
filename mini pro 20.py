d2ee25268e8f6d52d55b6ca848e36acb38954a49
swapnatirupathi <51733380+swapnatirupathi@users.noreply.github.com>
2025-09-25 15:44:34 +0530

    Add files via upload

---
+++ mini pro 20.py
@@ -0,0 +1,28 @@
+#inventory
+veg = ['brinjal','tamato','onion']
+quantity = [10,5,7]
+price = [60,15,20]
+#transactions
+while True:
+    print('1.admin')
+    print('2.user')
+    print('3.exait')
+
+    ch= input('choose any option(1.owner/2.customer/3.exit)')
+    if ch=='1':
+        print()
+        print('welcome to the shop owner: ')
+        chh=('choose any option(1.add item/2.remove/3.update/4.view/5.user details/6.view report/7.exit')
+    elif ch=='2':
+        pass
+
+    elif ch=='3':
+        print()
+        break
+    else:
+        print('choose the correct option')
+        
+        
+    
+         
+
